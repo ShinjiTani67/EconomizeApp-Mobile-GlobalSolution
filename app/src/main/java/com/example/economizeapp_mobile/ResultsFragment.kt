e2364167581c4d75668c7195f0f71c5f45047d96
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-18 10:44:38 -0300

    add do results fragment

---
+++ app/src/main/java/com/example/economizeapp_mobile/ResultsFragment.kt
@@ -0,0 +1,29 @@
+package com.example.economizeapp_mobile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ResultsFragment : Fragment() {
+    private var _binding : SignUpFragment? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_results, container, false)
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        _binding = null
+    }
+}
