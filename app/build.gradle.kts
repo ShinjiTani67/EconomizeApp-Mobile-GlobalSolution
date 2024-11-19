e2f8997adf4a1cfec5a5492df3ac05ab33890021
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-19 11:40:32 -0300

    implementação do navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.androidx.navigation.safeargs.kotlin)
 }
 
 android {
@@ -49,6 +50,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.navigation.ui)
+    implementation(libs.androidx.navigation.fragment)
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
 }
