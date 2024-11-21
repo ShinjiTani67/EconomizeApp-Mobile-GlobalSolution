1c3ea2cf662e66ed1feb975083dbfbc56952d9bc
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2024-11-21 08:46:15 -0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.androidx.navigation.safeargs.kotlin)
-    alias(libs.plugins.gms.google.services)
+    alias(libs.plugins.androidx.navigation.safeargs.kotlin) //navigation
+    alias(libs.plugins.gms.google.services) //firebase
 }
 
 android {
